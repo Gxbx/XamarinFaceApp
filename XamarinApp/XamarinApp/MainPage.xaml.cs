868a067a6da503aa6c09324bf27bb2b82d0d2fb0
Gxbx <gabo_pxndx@hotmail.com>
2019-10-30 11:57:34 -0500

    add Xamarin face recognition example

---
+++ XamarinApp/XamarinApp/MainPage.xaml.cs
@@ -0,0 +1,94 @@
+﻿using Newtonsoft.Json;
+using Plugin.Media;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XamarinApp.Models;
+
+namespace XamarinApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public string subscriptionKey = "58b0accfca894dcabe97e47ae143e845";
+
+        public string uriBase = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void btnPick_Clicked(object sender, System.EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+            try
+            {
+                var file = await CrossMedia.Current.PickPhotoAsync(new Plugin.Media.Abstractions.PickMediaOptions
+                {
+                    PhotoSize = Plugin.Media.Abstractions.PhotoSize.Medium
+                });
+                if (file == null) return;
+                imgSelected.Source = ImageSource.FromStream(() => {
+                    var stream = file.GetStream();
+                    return stream;
+                });
+                MakeAnalysisRequest(file.Path);
+            }
+            catch (Exception ex)
+            {
+                string test = ex.Message;
+            }
+
+        }
+
+
+        public async void MakeAnalysisRequest(string imageFilePath)
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+            string requestParameters = "returnFaceId=false&returnFaceLandmarks=false" +
+                "&returnFaceAttributes=age,gender,smile,glasses" ;
+
+            string uri = uriBase + "?" + requestParameters;
+            HttpResponseMessage response;
+            byte[] byteData = GetImageAsByteArray(imageFilePath);
+
+            using (ByteArrayContent content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                response = await client.PostAsync(uri, content);
+
+                string contentString = await response.Content.ReadAsStringAsync();
+                Console.Write(contentString);
+                List<ResponseModel> faceDetails = JsonConvert.DeserializeObject<List<ResponseModel>>(contentString);
+                if (faceDetails.Count != 0)
+                {
+                    lblTotalFace.Text = "Total de caras : " + faceDetails.Count;
+                    lblGender.Text = "Genero : " + faceDetails[0].faceAttributes.gender;
+                    lblAge.Text = "Edad : " + faceDetails[0].faceAttributes.age;
+                }
+
+            }
+        }
+        public byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            using (FileStream fileStream =
+                new FileStream(imageFilePath, FileMode.Open, FileAccess.Read))
+            {
+                BinaryReader binaryReader = new BinaryReader(fileStream);
+                return binaryReader.ReadBytes((int)fileStream.Length);
+            }
+        }
+    }
+}
---
+++ XamarinApp/XamarinApp/Models/Models.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace XamarinApp.Models
+{
+    public class ResponseModel
+    {
+        public string faceId { get; set; }
+        public FaceAttributes faceAttributes { get; set; }
+    }
+
+
+    public class FaceAttributes
+    {
+        public double smile { get; set; }
+        public string gender { get; set; }
+        public double age { get; set; }
+        public string glasses { get; set; }
+
+    }
+
+}
